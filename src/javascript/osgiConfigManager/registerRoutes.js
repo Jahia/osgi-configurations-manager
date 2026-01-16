0fb2776506c966434a507db2f9eaec173a5fb157
Dominique Gigon <dgigon@jahia.com>
2026-01-16 12:42:04 +0100

    Implement role-based access control and CSRF whitelist for osgi-configurations-manager

--- src/javascript/osgiConfigManager/registerRoutes.js
+++ src/javascript/osgiConfigManager/registerRoutes.js
@@ -5,6 +5,7 @@ import App from './App';
 export default function () {
     registry.add('adminRoute', 'osgi-configurations-manager', {
         targets: ['administration-server-configuration:99'],
+        requiredPermission: 'canManageOsgiConfigurations',
         label: 'osgi-configurations-manager:label',
         icon: null,
         isSelectable: true,
